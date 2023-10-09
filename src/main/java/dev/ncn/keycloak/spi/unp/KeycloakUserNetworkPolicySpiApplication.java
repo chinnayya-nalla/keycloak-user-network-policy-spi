1ab99ed1d93cc566a768f32422d2c86d1b37c185
Chinnayya Naidu Nalla <chinnayya.nalla@dtworld.ae>
2023-10-09 10:04:25 +0400

    Initial Commit

---
+++ src/main/java/dev/ncn/keycloak/spi/unp/KeycloakUserNetworkPolicySpiApplication.java
@@ -0,0 +1,13 @@
+package dev.ncn.keycloak.spi.unp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakUserNetworkPolicySpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakUserNetworkPolicySpiApplication.class, args);
+	}
+
+}
